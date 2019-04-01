43d925d68801024644fdd10d059f3494a793b88e
Antonio <antonio.toche@utec.edu.pe>
2019-04-01 11:44:33 -0500

    v2

--- main.cpp
+++ main.cpp
@@ -2,23 +2,34 @@
 #include <algorithm>
 using namespace std;
 
+void maxValue(int count, int temp)
+{
+    int temp;
+    if (count > temp)
+    {
+        temp = count;
+    }
+}
 void Logic(int MaxNumber, int MinNumber)
 {
-    for (int i = MaxNumber; i > MinNumber; i--)
+    for (int i = MaxNumber; i >= MinNumber; i--)
     {
-        do
+        int count = 0, value;
+        while (MaxNumber != 1)
         {
             if (MaxNumber % 2 == 1)
             {
+                count++;
                 MaxNumber = (3 * MaxNumber) + 1;
-                cout << MaxNumber << "\n";
             }
             if (MaxNumber % 2 == 0)
             {
+                count++;
                 MaxNumber = MaxNumber / 2;
-                cout << MaxNumber << "\n";
             }
-        } while (MaxNumber != 1);
+        }
+        count = count + 1;
+        maxValue(count);
         MaxNumber = i - MaxNumber;
     }
 }
@@ -39,5 +50,6 @@ int main()
     MaxNumber = MaxNumberFunc(input, input2);
     MinNumber = MinNumberFunc(input, input2);
     Logic(MaxNumber, MinNumber);
+    cout << temp;
     return 0;
 }
