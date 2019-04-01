41f21c90b03f3b6b834b6832c167752d95bb4f44
Antonio <antonio.toche@utec.edu.pe>
2019-04-01 16:44:37 -0500

    v4.0

--- main.cpp
+++ main.cpp
@@ -2,54 +2,59 @@
 #include <algorithm>
 using namespace std;
 
-void maxValue(int count)
-{
-    int temp;
-    if (count > temp)
-    {
-        temp = count;
-    }
-    cout << temp << endl;
-}
-void Logic(int MaxNumber, int MinNumber)
-{
-    for (int i = MaxNumber; i >= MinNumber; i--)
-    {
-        int count = 0, value;
-        while (MaxNumber != 1)
-        {
-            if (MaxNumber % 2 == 1)
-            {
-                count++;
-                MaxNumber = (3 * MaxNumber) + 1;
-            }
-            if (MaxNumber % 2 == 0)
-            {
-                count++;
-                MaxNumber = MaxNumber / 2;
-            }
-        }
-        count = count + 1;
-        maxValue(count);
-        MaxNumber = i - MaxNumber;
-    }
-}
-int MaxNumberFunc(int input, int input2)
-{
-    return max(input, input2);
-}
+void OrderMax(int &, int &);
+int Maxcycle(int, int);
+int Logic(int);
 
-int MinNumberFunc(int input, int input2)
-{
-    return min(input, input2);
-}
 int main()
 {
-    int input, input2, MaxNumber, MinNumber;
+    int input1, input2, numberCycles;
     cout << "Input: \n";
-    cin >> input >> input2;
-    MaxNumber = MaxNumberFunc(input, input2);
-    MinNumber = MinNumberFunc(input, input2);
-    Logic(MaxNumber, MinNumber);
-    return 0;
+    cin >> input1 >> input2;
+    numberCycles = Maxcycle(input1, input2);
+    cout << numberCycles;
+}
+
+void OrderMax(int &input1, int &input2)
+{
+    if (input2 < input1)
+    {
+        swap(input1, input2);
+    }
+}
+
+int Maxcycle(int input1, int input2)
+{
+    OrderMax(input1, input2);
+
+    int maxCycle = 0;
+    for (int i = input1; i <= input2; i++)
+    {
+        int cycle = Logic(i);
+        if (cycle > maxCycle)
+        {
+            maxCycle = cycle;
+        }
+    }
+
+    return maxCycle;
+}
+
+int Logic(int MaxNumber)
+{
+    int count = 1;
+    while (MaxNumber != 1)
+    {
+        if (MaxNumber % 2 == 1)
+        {
+            count++;
+            MaxNumber = (3 * MaxNumber) + 1;
+        }
+        if (MaxNumber % 2 == 0)
+        {
+            count++;
+            MaxNumber = MaxNumber / 2;
+        }
+    }
+    return count;
 }
